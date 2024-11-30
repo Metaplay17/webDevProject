d1bc4ed6c7f89c8d727da04cf2a1bc0def8bf633
Maxim <danin35@yandex.ru>
2024-11-30 12:20:47 +0300

    feat: complete left menu

--- code.js
+++ code.js
@@ -5,9 +5,10 @@ for (let i = 1; i <= 5; i++) {
     coffeeSelectors.push(document.getElementById(`${i}-coffee-type`))
 }
 
-let coffee = []
-let types = []
-let selectedTypes = [0, 1, 2, 3, 4]
+let coffee = [];
+let types = [];
+let selectedTypes = [0, 1, 2, 3, 4];
+let selectedBlock = -1;
 
 function loadCoffeeSync() {
     const xhr = new XMLHttpRequest();
@@ -26,13 +27,15 @@ function loadCoffeeSync() {
 function Init() {
     loadCoffeeSync();
     s = "";
+    let ind = 0;
     coffee.forEach(elem => {
         s += `<div class="coffee-card" id="${elem.type}-block">`
+        s += `<a name="${elem.type}"></a>`
         s += `<img src="${elem.image}"`
         s += `<p>${elem.name}</p>`
         s += `<div class="order-button-block">`
         s += `<div class="order-text-block">`
-        s += `<p>${elem.price}</p>`
+        s += `<p>${elem.price} RUB</p>`
         s += `</div>`
         s += `<button class="order-button">+</button>`
         s += `</div>`
@@ -46,8 +49,39 @@ Init();
 
 function Update() {
     for (let i = 0; i <= 4; i++) {
-        coffeeSelectors[i].innerHTML = `<p>${types[selectedTypes[i]].type}</p>`;
+        if (i == selectedBlock) {
+            let link = "";
+            coffee.forEach(elem => {
+                if (elem.type == types[selectedTypes[i]].type) {
+                    link = elem.image;
+                }
+            })
+            coffeeSelectors[i].classList.add("coffee-selected-block")
+            coffeeSelectors[i].classList.remove("coffee-selector-block")
+            coffeeSelectors[i].innerHTML = 
+            `
+            <a href="#${types[selectedTypes[i]].type}" onclick="SelectCoffee(${i})">
+                <img src="${link}">
+                <p>${types[selectedTypes[i]].type}</p>
+            </a>
+            `;
+        }
+        else {
+            coffeeSelectors[i].classList.remove("coffee-selected-block")
+            coffeeSelectors[i].classList.add("coffee-selector-block")
+            coffeeSelectors[i].innerHTML = 
+            `
+            <a href="#${types[selectedTypes[i]].type}" onclick="SelectCoffee(${i})">
+                <p>${types[selectedTypes[i]].type}</p>
+            </a>
+            `;
+        }
     }
+}
+
+function SelectCoffee(selectorInd) {
+    selectedBlock = selectorInd;
+    Update();
 }
 
 function ScrollDown() {
@@ -57,6 +91,7 @@ function ScrollDown() {
     for (let i = 0; i <= 4; i++) {
         selectedTypes[i] += 1;
     }
+    selectedBlock -= 1;
     Update();
 }
 
@@ -67,5 +102,6 @@ function ScrollUp() {
     for (let i = 0; i <= 4; i++) {
         selectedTypes[i] -= 1;
     }
+    selectedBlock += 1;
     Update();
 }
